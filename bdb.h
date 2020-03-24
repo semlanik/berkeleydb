7b28da5446c0506489615e1699f06aff23f00b5e
Alexey Edelev <semlanik@gmail.com>
2020-03-24 01:57:21 +0100

    Fix pointer related issue in c code

--- bdb.h
+++ bdb.h
@@ -11,13 +11,14 @@ extern int go_db_rename(DB *, char *, char *);
 extern int go_env_open(DB_ENV *, char *, u_int32_t, u_int32_t);
 extern int go_env_close(DB_ENV *, u_int32_t);
 
+// Read/write functions
+extern int go_db_put_string(DB *, char *, char *, u_int32_t);
+extern int go_db_get_string(DB *, char *, char *);
+extern int go_db_del_string(DB *, char *);
 
-// Convenience functions MAY BE REMOVED.
-int go_db_put_string(DB *, char *, char *, u_int32_t);
-int go_db_get_string(DB *, char *, char *);
-int go_db_del_string(DB *, char *);
-int go_db_cursor(DB *, DBC **);
-int go_cursor_get_next(DBC *, char *, char *);
-int go_cursor_get_prev(DBC *, char *, char *);
-int go_cursor_get_first(DBC *, char *, char *);
-int go_cursor_get_last(DBC *, char *, char *);
+// Cursor functions
+extern int go_db_cursor(DB *, DBC **);
+extern int go_cursor_get_next(DBC *, char *, char *);
+extern int go_cursor_get_prev(DBC *, char *, char *);
+extern int go_cursor_get_first(DBC *, char *, char *);
+extern int go_cursor_get_last(DBC *, char *, char *);
